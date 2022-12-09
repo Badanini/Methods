ae4a80abc85afe41cb4a72854633936bdac26834
Badanini <ivan.badanin1@gmail.com>
2022-12-10 00:05:05 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String args[]) {
+
+        hello();
+        welcome();
+        welcome();
+        hello();
+    }
+
+    static void hello() {
+        System.out.println("Hello");
+    }
+
+    static void welcome() {
+        System.out.println("Welcome to Java 10 DRRRRRR");
+    }
 }
