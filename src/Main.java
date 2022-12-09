34e6fa72c69f75b5b566939bc69d03181b18dcc4
Badanini <ivan.badanin1@gmail.com>
2022-12-10 01:32:16 +0300

    +sum

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    static int x = 3;
+    static int y = 4;
+    static int z = 2;
 
     public static void main(String args[]) {
 
@@ -6,6 +9,13 @@ public class Main {
         welcome();
         welcome();
         hello();
+        sum(y, z);
+        System.out.println();
+        int e = umnozh(x, y, z);
+        System.out.println(umnozh(x, x, x));
+        System.out.println(e);
+        System.out.println(umnozh(z, z, z));
+        System.out.println(sum(y, y));
     }
 
     static void hello() {
@@ -14,5 +24,17 @@ public class Main {
 
     static void welcome() {
         System.out.println("Welcome to Java 10 DRRRRRR");
+    }
+
+    static int sum(int a, int b) {
+        int summ = a + b;
+        System.out.println("x=" + a + " , y=" + b + " ,x+y=" + summ);
+        return summ;
+    }
+
+    static int umnozh(int a, int b, int c) {
+        int umn = a * b * c;
+        System.out.println(a + " *" + b + " *" + c + " =" + umn);
+        return umn;
     }
 }
